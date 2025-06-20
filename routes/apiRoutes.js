c9bfe441584a0621df893d81d8aaf7d6c0f498ca
Ogpavan <pawantwp@gmail.com>
2025-06-20 21:20:32 +0530

    changes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,16 +5,24 @@ const {
   createSession,
   listSessions,
   logoutAndDeleteSession,
-//   logoutSession, // Already imported
-//   deleteSession  // NEW: Delete session controller
+  //   logoutSession, // Already imported
+  //   deleteSession  // NEW: Delete session controller
 } = require("../controllers/sessionController");
 
 const { sendMessage } = require("../controllers/messageController");
+const {
+  getAllChats,
+  getMessages,
+} = require("../controllers/sessionController");
+const { getContacts } = require("../controllers/contactsController");
 
 router.get("/", (req, res) => res.send("WhatsApp Web.js API"));
 router.post("/create-session", createSession);
 router.get("/sessions", listSessions);
+router.get("/get-all-chats", getAllChats);
 router.post("/logout-and-delete", logoutAndDeleteSession);
+router.get("/get-messages", getMessages);
+router.get("/contacts", getContacts);
 // router.post("/logout", logoutSession); // Optional: Just disconnects
 // router.delete("/delete-session/:sessionId", deleteSession); // 👈 Add this
 
